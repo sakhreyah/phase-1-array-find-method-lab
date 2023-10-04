55ca858cd656c2f39dce5e8afb76a54a17e6fe0e
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-10-04 12:42:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array) {
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i].result === "W") {
+            return array[i].year
+        }
+    }
+    return undefined
+}
